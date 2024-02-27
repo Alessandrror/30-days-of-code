e4f0a9e10934de9bd8ecf057303f636279afba06
Alessandrror <ssandrorivas@gmail.com>
2024-02-27 14:46:06 -0600

    :heavy_plus_sign: Adding: challenge 05 - cs

---
+++ Day 05 - Loops/cs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        if (n < 2 && n > 20) {
+            return;
+        }
+
+        for (var i = 0; i < 10; i++) {
+            Console.WriteLine($"{n} x {i+1} = {n*(i+1)}");
+        }
+    }
+}
